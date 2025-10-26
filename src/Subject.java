84d447cb4b2530ec85532ffbc76df3d3e433b9cb
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-26 17:13:59 +0530

    Initial commit

---
+++ src/Subject.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+
+public class Subject {
+
+    private String name;
+    private int marks;
+
+    public Subject(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
