f41636d1517ddfb04171617a8737cad8d4d2403e
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-27 12:36:59 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -21,40 +21,64 @@ public class Main {
             System.out.println("Select your role: ");
 
             int choice= scanner.nextInt();
-
-            switch (choice){
+            char c;
+            switch (choice) {
                 case 1:
                     do {
                         System.out.println("1. Adding student");
                         System.out.println("2. View all Student");
                         System.out.println("3. Update student info");
-                        System.out.println("4. Enter marks");
+                        System.out.println("4. Update Marks: ");
                         System.out.println("5. Delete Student");
-                        System.out.println("6. Back");
                         System.out.println("Enter choice");
-                        int ch= scanner.nextInt();
+                        int ch = scanner.nextInt();
                         scanner.nextLine();
 
-                        switch (ch){
+                        switch (ch) {
                             case 1:
-                                student=adminService.createStudent();
+                                student = adminService.createStudent();
                                 studentList.add(student);
                                 break;
 
                             case 2:
                                 adminService.viewStudents(studentList);
+                                break;
+                            case 3:
+                                adminService.updateUsernamePassword(studentList);
+                                break;
 
+                            case 4: //update student marks
+                                adminService.updateStudentMarks(studentList);
+                                break;
+                            case 5: //delete student
+                                adminService.deleteStudent(studentList);
 
                         }
-                    }while();
-                }
+
+                        System.out.println("Do you what to continue the operations ?: ");
+                        while (true) {
+                            System.out.println("Enter Y/N");
+                            c = scanner.next().charAt(0);
+                            if (c == 'Y' || c == 'y' || c == 'n' || c == 'N') {
+                                break;
+                            } else {
+                                System.out.println("Enter valid input (y/n)");
+                            }
+
+                        }
+
+
+                    } while (c == 'Y' || c == 'y');
+                    break;
+                case 2:
+
+
+
+            }
+
             }
 
         }
-
-
-
-
 
     }
 }
