e920b3813f5d1f5415578b28e1d7cd2a1122d27a
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-27 12:51:35 +0530

    commit

--- src/StudentServices.java
+++ src/StudentServices.java
@@ -1,5 +1,42 @@
-
+import java.util.List;
+import java.util.Scanner;
 
 public class StudentServices {
+    Scanner scanner=new Scanner(System.in);
+    void checkDetails(List<Student>studentList){
+        System.out.println("Enter the studentId");
+        int id=scanner.nextInt();
+        boolean check=false;
 
+        for(Student student:studentList){
+
+            if(student.getStudentId()==id){
+                check=true;
+
+                System.out.println("Enter the username: ");
+                String userName=scanner.next();
+
+                if(student.getUsername().equals(userName)){
+                    System.out.println("Enter the password");
+                    String pass=scanner.next();
+
+                    if(student.getPassword().equals(pass)){
+                        System.out.println("Found");
+
+                        System.out.println("Your details: ");
+                        System.out.println(student);
+                    }
+                    else{
+                        System.out.println("Wrong password");
+                    }
+                }else {
+                    System.out.println("Wrong username");
+                }
+            }
+        }
+
+        if(!check){
+            System.out.println("Student not found with this id");
+        }
+    }
 }
